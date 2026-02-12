bccf3762a5d6322f074b9e3bf597d7bc853aae62
Avaneesh Shinde <avaneeshshinde23@gmail.com>
2026-02-12 19:16:33 +0530

    uhregkjiryfgr

---
+++ data/questions.js
@@ -0,0 +1,29 @@
+export const questions = {
+  "matter-in-our-surroundings": {
+    mcq40: [
+      {
+        q: "What is the SI unit of temperature?",
+        options: ["Celsius", "Kelvin", "Fahrenheit", "Joule"],
+        correct: 1,
+      },
+      {
+        q: "Which state of matter has fixed volume?",
+        options: ["Gas", "Plasma", "Solid", "Vapour"],
+        correct: 2,
+      },
+    ],
+
+    mcq80: [
+      {
+        q: "Why do gases diffuse faster than solids?",
+        options: [
+          "Higher density",
+          "Weak intermolecular forces",
+          "Lower temperature",
+          "Fixed shape",
+        ],
+        correct: 1,
+      },
+    ],
+  },
+};
