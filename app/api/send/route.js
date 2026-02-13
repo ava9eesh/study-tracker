a54ec0b0c12ce4b46f81d2ceb374d20ac65b7656
Avaneesh Shinde <avaneeshshinde23@gmail.com>
2026-02-13 19:02:59 +0530

    rehbkjgmjg

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -4,8 +4,6 @@ import { NextResponse } from "next/server";
 function setupVapid() {
   webpush.setVapidDetails(
     "mailto:your@email.com",
-    process.env.VAPID_PUBLIC_KEY,
-    process.env.VAPID_PRIVATE_KEY
   );
 }
 
