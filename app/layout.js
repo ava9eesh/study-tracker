4993ffe2dc9430a06d9f2b96739acd6a2d652224
Avaneesh Shinde <avaneeshshinde23@gmail.com>
2026-02-13 17:53:35 +0530

    rehgbejr

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className} bg-black text-white`}>
         {children}
+        <ServiceWorkerRegister />
       </body>
     </html>
   );
