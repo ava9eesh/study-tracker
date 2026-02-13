e780421313e0596717f4fdf3bbddf0481b168496
Avaneesh Shinde <avaneeshshinde23@gmail.com>
2026-02-13 23:26:27 +0530

    fix nextresponse

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,40 +1,5 @@
-import webpush from "web-push";
-import { adminDb } from "@/utils/firebaseAdmin";
-import { verifySignature } from "@upstash/qstash/nextjs";
 import { NextResponse } from "next/server";
 
-function setupVapid() {
-  webpush.setVapidDetails(
-    "mailto:your@email.com",
-    process.env.VAPID_PUBLIC_KEY,
-    process.env.VAPID_PRIVATE_KEY
-  );
+export async function GET() {
+  return NextResponse.json({ test: "working" });
 }
-
-const messages = [
-  "Did you study enough? Nothing is enough.",
-  "3 hours passed. Open the app.",
-  "Future you is watching.",
-  "No excuses.",
-];
-
-export const GET = verifySignature(async () => {
-  setupVapid();
-
-  const snapshot = await adminDb.collection("subscriptions").get();
-
-  const payload = JSON.stringify({
-    title: "Study Reminder 🔥",
-    body: messages[Math.floor(Math.random() * messages.length)],
-  });
-
-  for (const doc of snapshot.docs) {
-    try {
-      await webpush.sendNotification(doc.data(), payload);
-    } catch (err) {
-      console.error("Push failed:", err);
-    }
-  }
-
-  return NextResponse.json({ success: true });
-});
